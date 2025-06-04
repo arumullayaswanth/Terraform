1a08ca0e8ea21b6456475f7eefac8a93e0607043
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-06-04 15:36:15 +0530

    Update main.tf

--- 04-EC2_keypair/main.tf
+++ 04-EC2_keypair/main.tf
@@ -1,15 +1,16 @@
-resource "aws_key_pair" "name" {
-    key_name = "public"
-    public_key = file("~/.ssh/id_rsa.pub") #here you need to define public key file path
 
-  
+resource "aws_key_pair" "name" {
+  key_name = "public"
+  public_key = file("C:/Users/Yaswanth Reddy/.ssh/id_ed25519.pub")   #here you need to define public key file path
+
 }
 
 resource "aws_instance" "dev" {
-  ami                    = "ami-013e83f579886baeb"
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.name.key_name
-  tags = {
-    Name="key-tf"
-  }
+    ami = "ami-02457590d33d576c3"
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.name.key_name
+    tags = {
+      Name = "keys"
+    }
+
 }
