9652564ad7cbf1d455a3107ea82b343f5eddb2db
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-06-05 10:26:38 +0530

    Update main.tf

--- 05-terraform-ec2-s3-remote-backend-locking/main.tf
+++ 05-terraform-ec2-s3-remote-backend-locking/main.tf
@@ -1,14 +1,23 @@
-resource "aws_instance" "example" {
-  ami               = var.ami
-  instance_type     = var.instance_type
-  key_name          = var.key_name
-  availability_zone = var.ec2_az
+resource "aws_key_pair" "name" {
+  key_name = var.key_name
+  public_key = file("C:/Users/Yaswanth Reddy/.ssh/id_ed25519.pub")
 
-  tags = {
-    Name = var.ec2_name_tag
-  }
+}
+
+
+resource "aws_instance" "example" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = aws_key_pair.name.key_name
+    availability_zone = var.ec2_az
+
+    tags = {
+      Name = var.ec2_name_tag
+    }
+  
 }
 
 resource "aws_s3_bucket" "code_bucket" {
-  bucket = var.bucket_name
+    bucket = var.bucker_name
+  
 }
