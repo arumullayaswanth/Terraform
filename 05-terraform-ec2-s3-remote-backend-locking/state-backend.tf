8a55dfb3446028a1fb7f296b5c34c961038ba1bb
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-06-05 10:24:47 +0530

    Update state-backend.tf

--- 05-terraform-ec2-s3-remote-backend-locking/state-backend.tf
+++ 05-terraform-ec2-s3-remote-backend-locking/state-backend.tf
@@ -1,9 +1,11 @@
-terraform {
-  backend "s3" {
-    bucket         = "veeranareshitdevopsss"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock-dynamo"
-    encrypt        = true
-  }
-}
+#Temporarily Comment Out the Backend Block the backend is defined, comment out this block for now:
+ 
+ terraform {
+   backend "s3" {
+     bucket         = "terraform-state-lock-yaswanth6758546"
+     key            = "terraform.tfstate"
+     region         = "us-east-1"
+     dynamodb_table = "terraform-state-lock-dynamo"
+     encrypt        = true
+   }
+ }
