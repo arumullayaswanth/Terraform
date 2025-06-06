2b4e2d9454ff1bd2b8208ccaeb91b21b3cf53120
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-06-06 15:37:01 +0530

    Update outputs.tf

--- 08-terraform-rds-mysql-remote-null/outputs.tf
+++ 08-terraform-rds-mysql-remote-null/outputs.tf
@@ -1,19 +1,14 @@
 output "bastion_public_ip" {
-  description = "Public IP of the Bastion EC2 instance"
   value       = aws_instance.bastion.public_ip
+  description = "Public IP of Bastion Host"
 }
 
 output "rds_endpoint" {
-  description = "RDS MySQL endpoint"
   value       = aws_db_instance.mysql.address
+  description = "RDS MySQL endpoint"
 }
 
-output "rds_port" {
-  description = "RDS MySQL port"
-  value       = aws_db_instance.mysql.port
-}
-
-output "rds_db_name" {
-  description = "RDS MySQL DB Name"
-  value       = aws_db_instance.mysql.name
+output "rds_secret_arn" {
+  value       = aws_secretsmanager_secret.rds_secret.arn
+  description = "ARN of the database credentials secret"
 }
